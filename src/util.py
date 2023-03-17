f93c69f926e1ba54e94781bd7a54d5f1afd69da8
Always_begginer <beyounick00724@example.com>
2023-03-17 23:25:51 +0530

    Prediction pipline with flask

--- src/util.py
+++ src/util.py
@@ -8,6 +8,7 @@ from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -18,16 +19,16 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_model(x_train, y_train, x_test, y_test, models,param):
+def evaluate_model(x_train, y_train, x_test, y_test, models, param):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(x_train,y_train)
+            para = param[list(models.keys())[i]]
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(x_train, y_train)
             model.set_params(**gs.best_params_)
-            model.fit(x_train,y_train)
+            model.fit(x_train, y_train)
             # model.fit(x_train, y_train)  # Train model
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
@@ -35,5 +36,13 @@ def evaluate_model(x_train, y_train, x_test, y_test, models,param):
             test_model_score = r2_score(y_test, y_test_pred)
             report[list(models.keys())[i]] = test_model_score
             return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
